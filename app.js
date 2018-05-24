6c956250188a0dfff11b5b86793a83d6f0d8f463
Leonel Tejeda <leonelhtejeda@gmail.com>
2018-05-24 01:22:35 -0700

    express router and nodejs backend

--- app.js
+++ app.js
@@ -3,9 +3,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var compression = require('compression');
 
-var indexRouter = require('./routes/index');
+var mainRouter = require('./routes/main');
 var usersRouter = require('./routes/users');
+var aboutUs = require('./routes/about-us');
+var contact = require('./routes/contact');
+
+
 
 var app = express();
 
@@ -19,8 +24,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/', mainRouter);
 app.use('/users', usersRouter);
+app.use('/aboutUs', aboutUs);
+app.use('/contact', contact);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
