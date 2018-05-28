8c0109eedb59745741a44c543ed5282a73d0a601
Leonel Tejeda <leonelhtejeda@gmail.com>
2018-05-27 17:20:49 -0700

    backend completed

--- routes/contact.js
+++ routes/contact.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET contact page. */
 router.get('/', function(req, res, next) {
-  res.render('contact', { title: 'BellaMoves' });
+  res.render('contact');
 });
 
 module.exports = router;
--- routes/main.js
+++ routes/main.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('main', { title: 'BellaMoves' });
+  res.render('main',
+  { author: 'Bella',
+    headline: 'Welcome to Bella Moves'});
 });
 
 module.exports = router;
